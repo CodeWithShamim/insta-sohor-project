3959aba06ae7e618c64924d88a6e1c3046871abd
codewithshamim <shamimislamonline@gmail.com>
2022-03-10 22:38:26 +0600

    Reported post fixed bug

--- js/app.js
+++ js/app.js
@@ -150,10 +150,12 @@ const displayLikedPosts = () => {
 };
 
 const displayReportedPosts = () => {
+    document.getElementById("reported").innerHTML = '';
     const reportedPosts = getReportedPosts();
     reportedPosts.forEach((post) => {
         const div = createPost(post);
         document.getElementById("reported").appendChild(div);
+
     });
 };
 
